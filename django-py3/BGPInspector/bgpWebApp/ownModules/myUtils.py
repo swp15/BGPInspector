bfbcdbc1602218f9f8088e54dbf6a8872c511f22
Def <fajery@web.de>
2015-06-11 21:19:19 +0200

    VAST BGP json parsing tested from file included

--- django-py3/BGPInspector/bgpWebApp/ownModules/myUtils.py
+++ django-py3/BGPInspector/bgpWebApp/ownModules/myUtils.py
@@ -1,16 +1,25 @@
-from django.http import Http404
-from django.shortcuts import render
+#!python3
+__author__ = 'Fabrice Jean Ryba'
 
-# replace with logic TODO
-def is_valid_vast_query( query_string):
-	return True
+import ijson
 
-def refine_json_to_table_format( content_json):
-	if content_json['type'] == 'update':
-		header = content_json['header']
+def parse_json_to_table_format( content_json):
+	header = ['timestamp', 'source_ip', 'source_as', 'prefix', 'as_path', 'origin_as', 'origin', 'nexthop', 'local_pref', 'med', 'community', 'atomix_aggregate', 'aggregator'] #= content_json['header']
+	table = []
+	for json_object in content_json:
+		if 'type' in json_object['value'] and json_object['value']['type'] == 'bgpdump::announcement':
+			#event_id = content_json['id']
+			#timestamp = content_json['timestamp']
+			value_row = []
+			for value in json_object['value']['data']:
+				if value != type(list):
+					value_row.append( value)
+				else:
+					stringBuff = ''
+					for entry in value:
+						stringBuff += str(entry) + ' '
+					value_row.append( stringBuff)
+			table.append( value_row)				
+	return {'header':header, 'table':table}
 	
-		table = content_json['table']
-		
-		return {'header':header, 'table':table}
-	else:
-		return None
+
--- django-py3/BGPInspector/bgpWebApp/views.py
+++ django-py3/BGPInspector/bgpWebApp/views.py
@@ -1,3 +1,6 @@
+#!python3
+__author__ = 'Fabrice Jean Ryba'
+
 from django.shortcuts import render
 
 from django.http import HttpResponse, Http404, HttpResponseRedirect
@@ -7,6 +10,9 @@ import datetime
 
 from .forms import QueryForm
 from .ownModules import myUtils, HTTPHandler
+
+# for testing json
+import json
 
 # Create your views here.
 
@@ -19,38 +25,46 @@ def query(request):
 	# retrieve cookie?
 	if request.method == "POST":
 		form = QueryForm(request.POST)
-		if form.is_valid() and myUtils.is_valid_vast_query( form.cleaned_data['query']):	
+		if form.is_valid():	
 			query = form.cleaned_data['query']
-		
-			#return HttpResponse("testing cleaning: " + str(form.cleaned_data['query']))
-			# maybe hash query
 			httpHandler = HTTPHandler.HTTPHandler()
 			response = httpHandler.send_request(
 				'GET', 
 				'http://worldcup-simulator.de/json/rules/tournament:1', 
 				request_payload={'query':query})
-			time = datetime.datetime.now()
-			time = str(time)
-			content_emitter = {}
-			content_emitter['query'] = query
-			content_emitter['time'] = time
-			content_emitter['form'] = form
+			content_emitter = build_content_emitter( query, form)
 			if httpHandler.is_json( response):
-				content_json = httpHandler.extract_json_content( response)
-				# testing bgp update
-				content_json = {'type':'update','header':['Field Name', 'Marker', 'type'], 'table':[['einFieldName', 'einMarker', '5'], ['zweiFieldName', 'zweiMarker', '4']]}
-				content_json = myUtils.refine_json_to_table_format( content_json)
-				content_emitter['content_json'] = content_json
+				http_json_content = httpHandler.extract_json_content( response)
+				#import cProfile;cProfile.runctx('load_and_parse( response)', {'response' : response}, None )#cProfile.run('load_and_parse( response)')
+				http_json_content = load_and_parse( response)
+				content_emitter['http_json_content'] = http_json_content
 				content_emitter['is_json'] = True
-				return render(request, 'bgpWebApp/query.html' , content_emitter)
-			else: 
-				content_text = httpHandler.extract_text_content( response) 
-				content_emitter['content_text'] = content_text
+			else: 	
+				http_content = httpHandler.extract_text_content( response)
+				content_emitter['http_content'] = http_content
 				content_emitter['is_json'] = False
-				return render(request, 'bgpWebApp/query.html', content_emitter)
+			return render(request, 'bgpWebApp/query.html', content_emitter)
 	else:
 		form = QueryForm()
 	return render(request, 'bgpWebApp/query.html', {'form':form})
+
+def build_content_emitter( query, form):
+	content_emitter = {}
+	time = datetime.datetime.now()
+	time = str(time)
+	content_emitter = {}
+	content_emitter['query'] = query
+	content_emitter['time'] = time
+	content_emitter['form'] = form
+	return content_emitter
+
+def load_and_parse( response):
+	# ------------------- test ------------------------- #
+	with open( 'bgpWebApp/json_bgp_small.dump') as json_file:
+		http_json_content = json.load(json_file)
+	# -------------------------------------------------- #
+	http_json_content = myUtils.parse_json_to_table_format( http_json_content)
+	return http_json_content
 
 def result(request):
 	return render( request, 'bgpWebApp/result.html', {})
