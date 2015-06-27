60269f6ac0f4bcb791bdde37d00532e8b915dde4
Def <fajery@web.de>
2015-06-27 19:19:50 +0200

    added protocol button and header loading for dynamic tables. Only dynamic field filter is missing for issue #3

--- django-py3/BGPInspector/bgpWebApp/forms.py
+++ django-py3/BGPInspector/bgpWebApp/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
-class QueryForm(forms.Form):
-	query = forms.CharField(label="", max_length=200, widget=forms.TextInput(attrs={'size':'60'}))
+class Query_form(forms.Form):
+	protocol = forms.ChoiceField(label="", initial='bgp', choices=[('bgp', 'bgp'), ('pcap', 'pcap')])
+	query = forms.CharField(label="", max_length=200, widget=forms.TextInput(attrs={'size':'60'}), required=False)
+
+class Field_filter(forms.Form):
+	def __init__(self, field_choices, *args, **kwargs):
+		super(Field_filter, self).__init__(*args, **kwargs)
+		self.fields['field'].choices = field_choices
+
+	field = forms.ChoiceField(label="", initial=None, choices=('filter'), required=False)
