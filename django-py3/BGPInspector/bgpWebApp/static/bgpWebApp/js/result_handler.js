55884254ec30b2d92d616e7953f2e2d92c7e3f22
Def <fajery@web.de>
2015-06-30 13:53:25 +0200

    data table implementation added - not able to process received json objets

--- django-py3/BGPInspector/bgpWebApp/static/bgpWebApp/js/result_handler.js
+++ django-py3/BGPInspector/bgpWebApp/static/bgpWebApp/js/result_handler.js
@@ -1,30 +1,56 @@
-$.getScript("oboe-browser.js", function( data, textStatus, jqxhr ) {
-  console.log( data ); // Data returned
-    console.log( textStatus ); // Success
-      console.log( jqxhr.status ); // 200
-        console.log( "Load was performed." );
-        });
-
-
-
-
-function test(ding){
-    alert("funktioniert");
-    sendQuery();
+function process_query(query, representation, headers){
+	alert(query + ' ' + representation + ' ' + headers);
+	send_query(query, representation, headers);
 }
 
-function sendQuery(){
-    oboe('http://localhost:8888/?query=&time<now')
-   .done(
-   function(things) 
-   {
-   
-   $('#result_table').DataTable().row.add(things).draw();
-   console.log(things);
-   }
-   ).fail(function() 
-   {
-   console.log("fail")
-   }
-   );
+function send_query(query, representation, headers){
+	if (representation == 'table'){
+		build_header_table_in_result( headers, representation);
+	}	
+	oboe({
+   url: 'http://mobi3.cpt.haw-hamburg.de/API/query?query='+query+'&historical=true&limit=50',
+   method: 'GET',          // optional
+   //headers: Object,         // optional
+   //body: String|Object,     // optional
+   //cached: Boolean,         // optional
+   withCredentials: false
+	})
+	//oboe('http://fabrice-ryba.ddns.net/json_bgp_small.dump')
+	.done(
+		function(json_object) {
+			console.log(json_object);
+
+			if (representation == 'table'){
+				data_list = json_object['value']['data']
+				$(representation).row.add(data_list).draw();
+			}
+			if (representation == 'graph'){
+			} 
+					})
+	.fail(
+		function(err) {
+			console.log( err);
+		}
+	);
+}
+
+function build_header_table_in_result( headers, div_id){
+	table_header_string = build_table_string_from_array( headers);
+	document.getElementById('result').innerHTML = 
+		'<table id=\"'
+		+ div_id
+		+ '\" class=\"display\"> \n <thead> \n <tr>'
+		+ table_header_string
+		+ '\n </tr> \n </thead> \n </table>';
+	$(document).ready(function(){
+		$(div_id).DataTable();
+	});
+}
+
+function build_table_string_from_array( header_array){	
+	var table_header_string = '';
+	for (entry in header_array){
+		table_header_string += '\n <th>'+header_array[entry]+'</th>';
+	}
+	return table_header_string;
 }
