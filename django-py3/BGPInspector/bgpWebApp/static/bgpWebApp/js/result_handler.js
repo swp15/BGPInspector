541b8a099e8a28a700854674643b8c37a9f065f1
Def <fajery@web.de>
2015-06-30 17:04:36 +0200

    the table is now loading the data dynamically!

--- django-py3/BGPInspector/bgpWebApp/static/bgpWebApp/js/result_handler.js
+++ django-py3/BGPInspector/bgpWebApp/static/bgpWebApp/js/result_handler.js
@@ -1,40 +1,43 @@
 function process_query(query, representation, headers){
-	alert(query + ' ' + representation + ' ' + headers);
 	send_query(query, representation, headers);
 }
 
 function send_query(query, representation, headers){
+	var dt;
 	if (representation == 'table'){
-		build_header_table_in_result( headers, representation);
+		dt = build_header_table_in_result( headers, representation);
 	}	
 	oboe({
-   url: 'http://mobi3.cpt.haw-hamburg.de/API/query?query='+query+'&historical=true&limit=50',
-   method: 'GET',          // optional
-   //headers: Object,         // optional
-   //body: String|Object,     // optional
-   //cached: Boolean,         // optional
+   url: 'http://mobi3.cpt.haw-hamburg.de/API/query?query='+query+'&historical=true&limit=100',
    withCredentials: false
 	})
-	//oboe('http://fabrice-ryba.ddns.net/json_bgp_small.dump')
 	.done(
 		function(json_object) {
-			console.log(json_object);
-
+			//console.log(json_object.value.data);
 			if (representation == 'table'){
-				data_list = json_object['value']['data']
-				$(representation).row.add(data_list).draw();
+				var data_list = json_object.value.data;	
+				var diff =  headers.length - data_list.length;
+				if (diff > 0) {
+					for(i=0; i < diff; i++){
+						data_list.push("-");
+					}
+				}
+				//console.log(data_list);
+				$(representation).DataTable().row.add(data_list).draw();
 			}
 			if (representation == 'graph'){
 			} 
-					})
+		}
+	)
 	.fail(
 		function(err) {
-			console.log( err);
+			//console.log( err);
 		}
 	);
 }
 
 function build_header_table_in_result( headers, div_id){
+	var dt;
 	table_header_string = build_table_string_from_array( headers);
 	document.getElementById('result').innerHTML = 
 		'<table id=\"'
@@ -42,9 +45,11 @@ function build_header_table_in_result( headers, div_id){
 		+ '\" class=\"display\"> \n <thead> \n <tr>'
 		+ table_header_string
 		+ '\n </tr> \n </thead> \n </table>';
-	$(document).ready(function(){
-		$(div_id).DataTable();
+	dt = $(document).ready(function(){
+		dt = $(div_id).DataTable();
+		return dt;
 	});
+	return dt;
 }
 
 function build_table_string_from_array( header_array){	
