7315df1e9d70482e71debbae0638726df68c99e7
Def <fajery@web.de>
2015-12-10 01:56:05 +0100

    current tab highlightning

--- django-py3/BGPInspector/bgpWebApp/views.py
+++ django-py3/BGPInspector/bgpWebApp/views.py
@@ -10,11 +10,12 @@ from django.template import RequestContext, loader
 
 def index(request):
 	userIp = request.META['REMOTE_ADDR']
-	content_emitter = {'userIp': userIp}
+	content_emitter = {'userIp': userIp, 'current_link':'home'}
 	return render(request, 'bgpWebApp/index.html', content_emitter)
 
 def query(request):
-	return render(request, 'bgpWebApp/query.html', {})
+	content_emitter = {'current_link':'query'}
+	return render(request, 'bgpWebApp/query.html', content_emitter)
 
 def graph(request):
 	return render(request, 'bgpWebApp/graph.html', {})
